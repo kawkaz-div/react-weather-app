1c24354f7a36c693a18f06ae1efcfc8f8fc77858
kawkaz-div <kawawa465@gmail.com>
2026-05-12 01:30:22 +0900

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/react-weather-app/",
+  base: '/react-weather-app/',
   plugins: [react()],
 })
